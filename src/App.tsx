cf7f7700cec365ace1b1dd6db3468b033ddd5098
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-05-23 10:26:08 +0300

    feat: added lesson two components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-
+import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 
 function App() {
   return (
     <>
+      <FunctionalComponent />
+      <ClassComponent />
+      <ArrowFunctionalComponent />
     </>
   )
 }
